2c41819c916fffd3438e413117a2dcdd107e8395
Mathew Moon <mmoon@quinovas.com>
2021-04-23 09:26:05 -0500

    Fix version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup
 
 setup(
     name='appsync-router',
-    version='0.2.2-beta',
+    version='0.2.0-beta',
     description='A framework for building, testing, and deploying  an Appsync backend using AWS Lambda',
     author='Mathew Moon',
     author_email='mmoon@quinovas.com',
