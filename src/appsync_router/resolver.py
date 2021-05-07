bda656f3ba827816e8aae204526d9aa1cc6246ed
Mathew Moon <mmoon@quinovas.com>
2021-05-07 15:41:52 -0500

    File was a duplicate and not needed

--- src/appsync_router/resolver.py
+++
@@ -1,7 +0,0 @@
-from .router import Router
-from pkgutil import walk_packages
-
-resolvers = [x.name for x in walk_packages(path=["resolvers"])]
-router = Router()
-for x in resolvers:
-    exec(f"from resolvers.{x} import *")
