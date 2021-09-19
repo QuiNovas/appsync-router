9b240fd3d0454ffaee5feaf9bf3ea80542b03a76
Joseph Wortmann <jwortmann@quinovas.com>
2021-09-19 14:01:52 -0500

    multidispatch updates

--- appsync_router/__init__.py
+++ appsync_router/__init__.py
@@ -1,24 +1,31 @@
+import re
 from concurrent.futures import Executor
-from typing import Any, Callable, Dict, List, Set, Union
+from functools import partial
+from typing import Any, Callable, Union
 
+from multipledispatch import dispatch
+
+from .context import Info
 from .exceptions import (
     MultipleRoutesFoundExcepion,
     NoRouteFoundException,
     RouteAlreadyExistsException,
 )
+from .matches import DiscreteMatch, GlobMatch, PatternMatch
 from .routes import (
-    DiscreteMatch,
     DiscreteRoute,
-    GlobMatch,
     GlobRoute,
     MultiRoute,
-    PatternMatch,
     PatternRoute,
     Route,
     RouteHandler,
 )
 
-__ROUTES: Set[Route] = set()
+APPSYNC_ROUTER_NAMESPACE = dict()
+dispatch = partial(dispatch, namespace=APPSYNC_ROUTER_NAMESPACE)
+
+
+__ROUTES: set[Route] = set()
 
 
 def add_route(route: Route) -> Route:
@@ -27,6 +34,10 @@ def add_route(route: Route) -> Route:
             raise RouteAlreadyExistsException()
         __ROUTES.add(route)
     return route
+
+
+def get_routes() -> frozenset[Route]:
+    return frozenset(__ROUTES)
 
 
 def has_route(route: Route) -> bool:
@@ -50,6 +61,7 @@ def update_route(route: Route) -> Route:
 RouteHandlerDecorator = Callable[[RouteHandler], Route]
 
 
+@dispatch(DiscreteMatch)
 def discrete_route(match: DiscreteMatch) -> RouteHandlerDecorator:
     def register_route(handler: RouteHandler) -> Route:
         return add_route(DiscreteRoute(handler=handler, match=match))
@@ -57,13 +69,28 @@ def discrete_route(match: DiscreteMatch) -> RouteHandlerDecorator:
     return register_route
 
 
-def multi_route(matches: Set[DiscreteMatch]) -> RouteHandlerDecorator:
+@dispatch(str, str)
+def discrete_route(parentTypeName: str, fieldName: str) -> RouteHandlerDecorator:
+    return discrete_route(
+        DiscreteMatch(parentTypeName=parentTypeName, fieldName=fieldName)
+    )
+
+
+def multi_route(
+    match: Union[DiscreteMatch, tuple[str, str]],
+    *matches: Union[DiscreteMatch, tuple[str, str]],
+) -> RouteHandlerDecorator:
+    _matches = {DiscreteMatch(*match)}
+    for match in matches:
+        _matches.add(DiscreteMatch(*match))
+
     def register_route(handler: RouteHandler) -> Route:
-        return add_route(MultiRoute(handler=handler, matches=matches))
+        return add_route(MultiRoute(handler=handler, match=_matches))
 
     return register_route
 
 
+@dispatch(PatternMatch)
 def pattern_route(match: PatternMatch) -> RouteHandlerDecorator:
     def register_route(handler: RouteHandler) -> Route:
         return add_route(PatternRoute(handler=handler, match=match))
@@ -71,6 +98,31 @@ def pattern_route(match: PatternMatch) -> RouteHandlerDecorator:
     return register_route
 
 
+@dispatch(str, str)
+def pattern_route(parentTypeName: str, fieldName: str) -> RouteHandlerDecorator:
+    return pattern_route(re.compile(parentTypeName), re.compile(fieldName))
+
+
+@dispatch(re.Pattern, re.Pattern)
+def pattern_route(
+    parentTypeName: re.Pattern, fieldName: re.Pattern
+) -> RouteHandlerDecorator:
+    return pattern_route(
+        PatternMatch(parentTypeName=parentTypeName, fieldName=fieldName)
+    )
+
+
+@dispatch(str, re.Pattern)
+def pattern_route(parentTypeName: str, fieldName: re.Pattern) -> RouteHandlerDecorator:
+    return pattern_route(re.compile(parentTypeName), fieldName)
+
+
+@dispatch(re.Pattern, str)
+def pattern_route(parentTypeName: re.Pattern, fieldName: str) -> RouteHandlerDecorator:
+    return pattern_route(parentTypeName, re.compile(fieldName))
+
+
+@dispatch(GlobMatch)
 def glob_route(match: GlobMatch) -> RouteHandlerDecorator:
     def register_route(handler: RouteHandler) -> Route:
         return add_route(GlobRoute(handler=handler, match=match))
@@ -78,25 +130,34 @@ def glob_route(match: GlobMatch) -> RouteHandlerDecorator:
     return register_route
 
 
+@dispatch(str, str)
+def glob_route(parentTypeName: str, fieldName: str) -> RouteHandlerDecorator:
+    return glob_route(GlobMatch(parentTypeName=parentTypeName, fieldName=fieldName))
+
+
 def route_event(
-    event: Union[List[Dict], Dict],
+    event: Union[list[dict], dict],
     *,
     default_route: Route = Route(),
-    executor: Executor = None
+    executor: Executor = None,
+    short_circuit: bool = True,
 ) -> Any:
     info = event["info"] if isinstance(event, dict) else event[0]["info"]
-    route: Route = None
-    context_routes = {route for route in frozenset(__ROUTES) if route.match(info)}
-    if not len(context_routes):
+    context_route: Route = None
+    for route in frozenset(__ROUTES):
+        if route.match(info):
+            if context_route:
+                raise MultipleRoutesFoundExcepion(info=info)
+            context_route = route
+            if short_circuit:
+                break
+    if not context_route:
         if default_route:
-            route = default_route
-        raise NoRouteFoundException(info=info)
-    elif len(context_routes) == 1:
-        route = context_routes.pop()
-    else:
-        raise MultipleRoutesFoundExcepion(info=info)
+            context_route = default_route
+        else:
+            raise NoRouteFoundException(info=info)
     return (
-        route(event)
+        context_route(event)
         if isinstance(event, dict)
-        else list((executor.map if executor else map)(route, event))
+        else list((executor.map if executor else map)(context_route, event))
     )
